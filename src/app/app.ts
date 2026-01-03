a4296b8c7d43ac157982ec30806596002e96b9ba
SHAH RUKH MEHDI <srmehdi59@gmail.com>
2026-01-04 04:00:15 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('Angular-21-Project');
+  protected readonly title = signal('SRM');
 }
